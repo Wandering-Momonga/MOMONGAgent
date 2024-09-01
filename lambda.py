4645f27227988499812dfc2f4ae4f0e56104891c
saito <t-saito@bfts.co.jp>
2024-09-02 08:59:57 +0900

    Latest

--- lambda.py
+++ lambda.py
@@ -13,29 +13,37 @@ ssm = boto3.client('ssm')
 # 最後に処理したメッセージのタイムスタンプを保存する変数
 last_processed_ts = 0.0
 
+#Slackからのメッセージに「モモンガくん」が含まれている場合のみ実行
+def is_reaction_message(body: dict) -> bool:
+    return 'モモンガくん' in body['event']['text']
+
+#Bedrock呼び出し関数
 def invoke_bedrock(prompt: str):
     chat = ChatBedrock(
-        model_id="anthropic.claude-3-sonnet-20240229-v1:0",
+        model_id="anthropic.claude-3-haiku-20240307-v1:0",
         model_kwargs={"max_tokens": 500},
     )
     messages = [
         SystemMessage(content="""
                       #役割
-                      あなたは、可愛いモモンガです。以下の{制約条件}に従って、最高の返答をしてください。
+                      あなたは、とても親しみやすいモモンガです。
+                      #命令文
+                      以下の{制約条件}に従って、最高の返答をしてください。
                       #制約条件
-                      ・一人称は「僕」であること
-                      ・丁寧語は使わないこと
-                      ・愛嬌を持って答えること
-                      ・正確性より穏やかさを重視すること
+                      ・一人称は「僕」である
+                      ・丁寧語は使わない
+                      ・可愛げがある
+                      ・正確性より穏やかさを重視する
                       """),
         HumanMessage(content=prompt),
     ]
     response = chat.invoke(messages)
     return response.content
-    
+
+#SSMのパラメータストアからSlackAppのOAuthトークンを呼び出し    
 def get_token() -> str:
     res = ssm.get_parameter(
-        Name='/Slack/Token/BetaMOMONGA',
+        Name='/Slack/Token/MOMONGAgent',
         WithDecryption=True
     )
     return res['Parameter']['Value']
@@ -67,7 +75,8 @@ def message_slack(body: dict, token: str, message: str) -> None:
     result = invoke_bedrock(text)
     data = {
         'channel': channel,
-        'text': result
+        'text': result,
+        'thread_ts': body['event']['event_ts']
     }
     try:
         response = requests.post(url, headers=headers, data=json.dumps(data))
@@ -89,10 +98,14 @@ def lambda_handler(event: dict, context: dict):
             'body': 'Bad Request'
         }
     
+    # APIリクエストを再実行させない魔法の呪文
     if 'headers' in event and 'X-Slack-Retry-Num' in event['headers']:
         return {
             'statusCode': 200
         }
+
+    if is_reaction_message(body) is False:
+        return
 
     token = get_token()
     message_slack(body, token, 'message')
